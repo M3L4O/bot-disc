9fb48bb4d07c1c0cbc771c84758d13c1efd7ab0c
João Victor Melo <joaovictor.slv@ufpi.edu.br>
2022-03-19 18:31:21 -0300

    feedback pertubação

--- Discord_Bot.py
+++ Discord_Bot.py
@@ -65,7 +65,7 @@ async def perturbar(ctx, arg : Member):
   spam ='\t'.join(f"<@{arg.id}>" for _ in range(10))
   for _ in range(10):
     await arg.send(spam)
-  await ctx.send(f"<A{arg.id}> foi pertubado com sucesso.")
+  await ctx.send(f"<@{arg.id}> foi pertubado com sucesso.")
   
 
 @bot.command()
